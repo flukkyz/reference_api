e91d60baac310ca4c62499dbedfd2d113affcf08
KUNAKORN KATASAI <kunakorn.k@cmu.ac.th>
2021-07-02 10:32:00 +0700

    manage logger

---
+++ config/logger.js
@@ -0,0 +1,16 @@
+const morgan = require('morgan')
+const path = require('path')
+const rfs = require('rotating-file-stream')
+
+morgan.token('date', function() {
+  const p = new Date().toString().replace(/[A-Z]{3}\+/,'+').split(/ /);
+  return( p[2]+'/'+p[1]+'/'+p[3]+':'+p[4]+' '+p[5] );
+});
+
+const accessLogStream = rfs.createStream('access.log', {
+  size: "10M",
+  interval: '1d',
+  path: path.join(__dirname, '../logs')
+})
+
+module.exports = morgan('combined', { stream: accessLogStream })
--- server.js
+++ server.js
@@ -1,31 +1,19 @@
 require('dotenv').config()
 const express = require('express')
-const morgan = require('morgan')
 const cors = require('cors')
-const path = require('path')
-const dayjs = require('dayjs')
-const rfs = require('rotating-file-stream')
 
 const { notFound, handleError } = require('./middlewares')
 const routes = require('./routes')
+const logger = require('./config/logger')
 
 const port = process.env.PORT || 5000;
 const apiDir = process.env.API_DIR || '/api';
 
 const app = express()
 
-morgan.token('date', function() {
-  var p = new Date().toString().replace(/[A-Z]{3}\+/,'+').split(/ /);
-  return( p[2]+'/'+p[1]+'/'+p[3]+':'+p[4]+' '+p[5] );
-});
-
 app.use(express.json())
 app.use(cors())
-var accessLogStream = rfs.createStream('access.log', {
-  interval: '1d',
-  path: path.join(__dirname, 'logs')
-})
-app.use(morgan('combined', { stream: accessLogStream }))
+app.use(logger)
 app.use(express.urlencoded({
   extended: false
 }))
