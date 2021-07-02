bb9fcf91731fae2fa10cdae8f0240a3dd8a7e997
KUNAKORN KATASAI <kunakorn.k@cmu.ac.th>
2021-07-02 19:02:32 +0700

    [bug]

--- config/logger.js
+++ config/logger.js
@@ -2,13 +2,6 @@ const morgan = require('morgan')
 const path = require('path')
 const rfs = require('rotating-file-stream')
 
-// if(process.env.NODE_ENV === 'development'){
-//   morgan.token('date', () => {
-//     const p = new Date().toString().replace(/[A-Z]{3}\+/,'+').split(/ /);
-//     return( p[2]+'/'+p[1]+'/'+p[3]+':'+p[4]+' '+p[5] );
-//   });
-// }
-
 morgan.token('level', () => {
   return 'info';
 });
@@ -20,4 +13,4 @@ const accessLogStream = rfs.createStream('access.log', {
   path: path.join(__dirname, '../logs')
 })
 
-module.exports = morgan(':level :remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] ":referrer" ":user-agent" - :response-time ms', { stream: accessLogStream })
+module.exports = morgan('combined', { stream: accessLogStream })
